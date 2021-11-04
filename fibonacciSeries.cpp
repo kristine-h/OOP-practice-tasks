de20f47c4493ef6c24dde44c4c37722132f99b12
kristine-h <89122214+kristine-h@users.noreply.github.com>
2021-11-04 20:31:36 +0400

    Update fibonacciSeries.cpp

--- fibonacciSeries.cpp
+++ fibonacciSeries.cpp
@@ -4,14 +4,13 @@ class Fibonacci {
 public:
 	Fibonacci() { x_ = 0; y_ = 1; z_ = x_ + y_;}
 	void printFibonacciNumbers(int);
-	
 private:
 	int x_;
 	int y_;
 	int z_;
 };
 
-void Fibonacci::printFibonacciNumbers(int count){
+void Fibonacci::printFibonacciNumbers(int count) {
 	std::cout << "The first " << count <<" fibonacci numbers are: " << x_ << " " << y_ << " ";
 	while (count != 2) { 
 		std::cout << z_ << " ";
@@ -28,4 +27,3 @@ int main() {
 	fibonacci_series.printFibonacciNumbers(8);
 	return 0;
 }
-
