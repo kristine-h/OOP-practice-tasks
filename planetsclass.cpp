4931fcfbc222e715378d45257c9aed0e5bff1ae1
kristine-h <89122214+kristine-h@users.noreply.github.com>
2021-11-04 20:28:50 +0400

    Update planetsclass.cpp

--- planetsclass.cpp
+++ planetsclass.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 class Planets {
-
 public:
 	void set_name(const std::string& name) { name_ = name;}
 	void set_radius(const double& radius) { radius_ = radius;}
@@ -9,21 +8,17 @@ public:
 	void set_mass(const double& mass) { mass_ = mass;}
 	std::string get_name() const { return name_;}
 	double get_gravity() const { return calculate_gravity();}
-	
 private:
 	std::string name_;
 	double distance_from_the_Sun;
 	double radius_;
 	double mass_;
 	const double G_ = 6.6743E-11;
-
 	double calculate_gravity() const { return (G_ * mass_) / (radius_ * radius_);}
 
 };
 
-
 int main() {
-
 	Planets planet0;
 	planet0.set_name("Earth");
 	planet0.set_radius(6.371);
@@ -32,7 +27,6 @@ int main() {
 	std::cout << "\nThe Gravity of " << planet0.get_name() << " is " << planet0.get_gravity() << " meters per second squared \n\n";
 
 	Planets planet1;
-
 	std::string name;
 	std::cout << "Enter the name of your favorite planet: ";
 	std::cin >> name;
@@ -52,8 +46,6 @@ int main() {
 	std::cout << "Enter the mass of " << planet1.get_name() << " in kgs: ";
 	std::cin >> mass;
 	planet1.set_mass(mass);
-
 	std::cout << "The gravity of " << planet1.get_name() << " is " << planet1.get_gravity() << std::endl;
-
 	return 0;
 }
